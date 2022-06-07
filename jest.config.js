8d3e44a01e9c6f1c0bd68b1ac4f9e9b5e0d82be5
za-huangjian <jian.huang@zatech.com>
2022-06-07 09:55:21 +0800

    chore: format with newer toolchain

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   transform: {
-    '\\.(ts|tsx|js|jsx)$': 'babel-jest',
+    '\\.(ts|tsx|js|jsx)$': 'esbuild-jest',
   },
-  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
+  moduleFileExtensions: ['js', 'ts'],
   moduleDirectories: ['node_modules'],
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
   coverageDirectory: 'coverage',
   coverageReporters: ['lcov', 'html'],
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/__tests__/**/*.spec.{js,jsx,ts,tsx}'],
+  testMatch: ['<rootDir>/__tests__/**/*.spec.{js,ts}'],
   testPathIgnorePatterns: ['/node_modules/'],
 };
